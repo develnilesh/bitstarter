9d5639a0f5dd81f8992ef0f9955026763262e2f8
Nilesh Singh <devel.nilesh@gmail.com>
2013-07-15 07:55:56 +0000

    fix web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var filename = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = filename.readFile('index.html');
+  var buffer = filename.readFileSync('index.html');
   var str = buffer.toString();
   response.send(str);
 });
